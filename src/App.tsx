cc11a4e5ff3ea0730322febaa1dde178e83b84e0
Osman Topuz <osmantopuz98@gmail.com>
2024-07-30 15:24:11 +0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="container">
       <PasswordCard/>
+
+      <div className='footer'>
+        <span className='info'>This project made by <a target='_blank' className='link' href="https://github.com/otpz">Osman TOPUZ</a></span>
+      </div>
     </div>
   );
 }
